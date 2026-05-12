d9ff3100c90acc73710cb2d0f03e62e5d1cfbc25
web dev <154662817+D3XH3R@users.noreply.github.com>
2026-05-12 10:39:58 +0300

    Transitioning from a simple function to a class

--- cl!ent/cl!ent.cpp
+++ cl!ent/cl!ent.cpp
@@ -12,7 +12,7 @@
 
 #define BACKLOG 10
 #define PORT "1080"
-#define NODE "10.64.115.48"
+#define NODE "10.56.57.48"
 using namespace std;
 
 void checker() {
@@ -104,9 +104,122 @@ void SimpleClient() {
     }
     closesocket(sockfd);
 };
+
+class client {
+protected:
+    struct addrinfo hints, * res, * p;
+    int result;
+    int sockfd;
+    fd_set readfd;
+    fd_set writefd;
+    char buf[1024];
+    char s[INET6_ADDRSTRLEN];
+    string msg = "";
+
+private:
+	// member functions
+    void checker() {
+        WSAData wsadata;
+        if (WSAStartup(MAKEWORD(2, 2), &wsadata) != 0) {
+            cout << "WSAStartup failed." << endl;
+        }
+        else {
+            cout << "WSAStartup works fine." << endl;
+        }
+    }
+    void* get_addrinf0(struct sockaddr* sa) {
+        if (sa->sa_family == AF_INET)
+            return &((struct sockaddr_in*)sa)->sin_addr;
+        if (sa->sa_family == AF_INET6)
+            return &((struct sockaddr_in6*)sa)->sin6_addr;
+        return nullptr;
+    };
+public:
+	// constructor and destructor
+    client(const char* ip_param, const char* port_param) {
+		checker();
+        cout << "CLIENT" << endl;
+        memset(&hints, 0, sizeof hints);
+        hints.ai_family = AF_UNSPEC;
+        hints.ai_socktype = SOCK_STREAM;
+        result = getaddrinfo(ip_param, port_param, &hints, &res);
+        if (result != 0) {
+            cerr << "getaddrinfo error: " << WSAGetLastError() << endl;
+            return;
+        }
+        for (p = res; p != NULL; p = p->ai_next) {
+            sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
+            if (sockfd == INVALID_SOCKET) {
+                cerr << "socket error: " << WSAGetLastError() << endl;
+                continue;
+            }
+            if (connect(sockfd, p->ai_addr, p->ai_addrlen) == SOCKET_ERROR) {
+                cerr << "connect error: " << WSAGetLastError() << endl;
+                closesocket(sockfd);
+                continue;
+            }
+            break;
+        }
+        if (p == NULL) {
+            cerr << "Failed to connect to server: " << WSAGetLastError() << endl;
+            return;
+        }
+        freeaddrinfo(res);
+        if ((send(sockfd, "Hello there", 11, 0)) == SOCKET_ERROR)
+            cerr << "send error: " << WSAGetLastError() << endl;
+	};
+    ~client() {
+        closesocket(sockfd);
+        WSACleanup();
+    }
+	// member functions continues
+private:
+    void sendMessage() {
+        int len = (int)msg.size(), bytes_sent;
+        if ((bytes_sent = send(sockfd, msg.c_str(), len, 0)) == SOCKET_ERROR)
+            cerr << "send error: " << WSAGetLastError() << endl;
+        msg = "";
+	}
+public:
+    void receiveMessage() {
+		fd_set readfd;
+		fd_set writefd;
+        FD_ZERO(&readfd);
+        FD_ZERO(&writefd);
+        FD_SET(sockfd, &readfd);
+        FD_SET(sockfd, &writefd);
+		int rv = select(sockfd + 1, &readfd, &writefd, NULL, NULL); // I hardcoded the first parameter to be sockfd + 1 because it's the only socket we're monitoring, but in a more complex client with multiple sockets, this should be set to the highest socket descriptor plus one.
+        if (rv == SOCKET_ERROR) {
+            cout << "select error: " << WSAGetLastError() << endl;
+            return;
+        }
+           
+        if (FD_ISSET(sockfd, &readfd)) {
+            int recievLen = recv(sockfd, buf, 1023, 0);
+            if (recievLen == SOCKET_ERROR)
+                cerr << "receive error: " << WSAGetLastError() << endl;
+            buf[recievLen] = '\0';
+            inet_ntop(p->ai_family, get_addrinf0((struct sockaddr*)p->ai_addr), s, sizeof s);
+            cout << s << ": " << buf << endl;
+            buf[0] = '\0';
+            cout << "-----------------------------" << endl;
+            cout << "Typing: ";
+            getline(cin, msg);
+        };
+        if (msg != "" && FD_ISSET(sockfd, &writefd)) {
+            sendMessage();
+        }
+    }
+};
+
+
 int main()
 {
-    checker();
-    SimpleClient();
-    WSACleanup(); // clean up after we're done with Winsock
+ //   checker();
+ //   SimpleClient();
+	//WSACleanup(); // clean up after we're done with Winsock
+	client myClient(NODE, PORT);
+    while (true) {
+        myClient.receiveMessage();
+	}
 }
--- server/server.cpp
+++ server/server.cpp
@@ -8,11 +8,12 @@
 #include <thread>
 #include <string>
 #include <cstring>
+#include <vector>
 #pragma comment(lib, "Ws2_32.lib")
 
 #define BACKLOG 10
 #define PORT "1080"
-#define NODE "10.64.115.48"
+#define NODE "10.56.57.48"
 using namespace std;
 
 // This function checks if WSAStartup works fine and prints the result.
@@ -65,8 +66,10 @@ void SimpleServer() {
         }
         if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(char)) == SOCKET_ERROR)
             continue;
-        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == SOCKET_ERROR)
+        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == SOCKET_ERROR) {
+            cout << "bind error: " << WSAGetLastError() << endl;
             continue;
+        }
         break;
     }
     if (p == NULL) {
@@ -122,11 +125,138 @@ void SimpleServer() {
     }
 };
 
+class printMessage {
+public:
+    int count = 0;
+public:
+    printMessage(int count_param) : count(count_param) {};
+    void print() {
+        cout << count << endl;
+    }
+
+};
+
+class server {
+protected:
+	SOCKET server_socket;
+	vector<SOCKET> clients;
+    struct addrinfo hints, * res, * p;
+    struct sockaddr_storage their_addr;
+    int sin_size;
+    int result;
+    int listening;
+    char s[INET6_ADDRSTRLEN];
+    char buf[1024];
+
+public:
+    server(const char* ip_param, const char* port_param) {
+        const char yes = '1';
+        memset(&hints, 0, sizeof hints);
+        hints.ai_family = AF_UNSPEC;
+        hints.ai_flags = AI_PASSIVE;
+        hints.ai_socktype = SOCK_STREAM;
+
+        if ((result = getaddrinfo(ip_param, port_param, &hints, &res)) != 0) {
+            cout << "getaddrinfo error: " << WSAGetLastError() << endl;
+        }
+        for (p = res; p != NULL; p = p->ai_next) {
+            if ((server_socket = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == INVALID_SOCKET) {
+                cout << "socket error: " << WSAGetLastError() << endl;
+                continue;
+            }
+            if (setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(char)) == SOCKET_ERROR)
+                continue;
+            if (bind(server_socket, p->ai_addr, p->ai_addrlen) == SOCKET_ERROR)
+                continue;
+            break;
+        }
+        if (p == NULL) {
+            cout << "An error happen while setting up the socket and binding it: " << WSAGetLastError() << endl;
+            return;
+        }
+
+        freeaddrinfo(res);
+        if ((listening = listen(server_socket, BACKLOG)) == SOCKET_ERROR) {
+            cout << "listen error: " << WSAGetLastError() << endl;
+        }
+		cout << "Waiting for connections..." << endl;
+	};
+    ~server() {
+        closesocket(server_socket);
+        for(SOCKET& s: clients)
+			closesocket(s);
+        cout << "closed all sockets" << endl;
+	};
+	// This function gets the address information from a sockaddr structure, depending on the address family (IPv4 or IPv6).
+    void* get_addrinf0(struct sockaddr* sa) {
+        if (sa->sa_family == AF_INET)
+            return &((struct sockaddr_in*)sa)->sin_addr;
+        if (sa->sa_family == AF_INET6)
+            return &((struct sockaddr_in6*)sa)->sin6_addr;
+        return nullptr;
+    };
+	// This function accepts incoming connections and adds them to the clients vector.
+    void acceptConnections() {
+            sin_size = sizeof their_addr;
+            SOCKET new_sockfd = accept(server_socket, (struct  sockaddr*)&their_addr, &sin_size);
+            if (new_sockfd == INVALID_SOCKET) {
+                cout << "accept error: " << WSAGetLastError() << endl;
+                return;
+            }
+            inet_ntop(their_addr.ss_family, get_addrinf0((struct sockaddr*)&their_addr), s, sizeof s);
+            cout << "New connection from: " << s << endl;
+            clients.push_back(new_sockfd);
+    }
+    void broadcastMessage(const char* message) {
+        for (SOCKET& client : clients) {
+            int len = (int)strlen(message), bytes_sent;
+            if ((bytes_sent = send(client, message, len, 0)) == SOCKET_ERROR)
+                cerr << "send error: " << WSAGetLastError() << endl;
+        }
+		buf[0] = '\0';
+	}
+    void receiveMessages() {
+        fd_set readfds;
+        while (true) {
+            FD_ZERO(&readfds);
+            for (SOCKET& client : clients) {
+                FD_SET(client, &readfds);
+            }
+            int n = clients.back() + 1;
+            int rv = select(n, &readfds, NULL, NULL, NULL);
+            if (rv == SOCKET_ERROR) {
+                cout << "select error: " << WSAGetLastError() << endl;
+                return;
+            }
+            for (SOCKET& client : clients) {
+                if (FD_ISSET(client, &readfds)) {
+                    int bytes_received = recv(client, buf, sizeof buf, 0);
+                    if (bytes_received > 0) {
+                        buf[bytes_received] = '\0';
+                        cout << "Message from client: " << buf << endl;
+                        broadcastMessage(buf);
+                    } else if (bytes_received == 0) {
+                        cout << "Client disconnected." << endl;
+                        closesocket(client);
+                        clients.erase(remove(clients.begin(), clients.end(), client), clients.end());
+                    } else {
+                        cout << "recv error: " << WSAGetLastError() << endl;
+                    }
+                }
+            }
+        }
+	}
+};
 
 int main()
 {
     checker();
-    SimpleServer();
+    server myServer("10.56.57.48", "1080");
+    while (true) {
+        myServer.acceptConnections();
+        //thread receiveThread(&server::receiveMessages, &myServer);
+        //receiveThread.detach();
+    }
     WSACleanup(); // clean up after we're done with Winsock
 }
 
